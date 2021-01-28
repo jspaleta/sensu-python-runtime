7ffe318e55dd461d41ebd7eb5facb32bbe8613a4
siva9427 <sivasukumarreddy.poreddy@objectrocket.com>
2021-01-29 01:11:55 +0530

    OR5-246 commit

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -1,16 +1,17 @@
 ASSET_FILE_NAME=sensu-python-runtime_${ASSET_VERSION}_python-${PYTHON_VERSION}_${OSVERSION}_linux_amd64.tar.gz
 S3_LOCATION=s3://${S3_BUCKET}${S3_FOLDER}
+OSDISTRO=$(printf '%s' "$OSVERSION" | sed 's/[0-9]*//g')
+export S3_FILE_URL=https://${S3_BUCKET}.s3.amazonaws.com${S3_FOLDER}${ASSET_FILE_NAME}
+export OSDISTRO=$OSDISTRO
 pip install --user awscli
 export PATH=/var/lib/jenkins/.local/bin:$PATH
 docker build --build-arg "ASSET_VERSION=${ASSET_VERSION}" --build-arg "PYTHON_VERSION=${PYTHON_VERSION}" -t sensu-python-runtime:${PYTHON_VERSION}-${OSVERSION} -f Dockerfile.${OSVERSION} .
 docker run -itd --name sensu-python-runtime_${PYTHON_VERSION}-${OSVERSION} sensu-python-runtime:${PYTHON_VERSION}-${OSVERSION}
 docker cp sensu-python-runtime_${PYTHON_VERSION}-${OSVERSION}:/assets/${ASSET_FILE_NAME} .
 docker rm -f sensu-python-runtime_${PYTHON_VERSION}-${OSVERSION}
-echo "Uploading archive to S3"
+echo "Uploading sensu asset to S3 bucket: ${S3_BUCKET}"
 SSUM=$(sha512sum ${ASSET_FILE_NAME} | cut -d ' ' -f 1)
 export SSUM=$SSUM
-aws s3 cp ${ASSET_FILE_NAME} ${S3_LOCATION}
-export S3_FILE_URL=https://${S3_BUCKET}.s3.amazonaws.com${S3_FOLDER}${ASSET_FILE_NAME}
+aws s3 cp ${ASSET_FILE_NAME} ${S3_LOCATION} --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 rm -rf ${ASSET_FILE_NAME}
-export OSDISTRO=${OSVERSION//[[:digit:]]/}
 echo $(envsubst < sensu-python-runtime-configuration-template.json) > sensu-python-runtime-${PYTHON_VERSION}-${OSVERSION}.json
