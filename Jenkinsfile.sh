3fdb8871203d364ae9f966b41880da0d0e863bb2
siva9427 <sivasukumarreddy.poreddy@objectrocket.com>
2021-01-29 21:16:03 +0530

    OR5-246 commit

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -15,4 +15,6 @@ export SSUM=$SSUM
 aws s3 cp ${ASSET_FILE_NAME} ${S3_LOCATION} --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 rm -rf ${ASSET_FILE_NAME}
 echo $(envsubst < py3-runtime/asset.yml) > py3-runtime/asset.yml
-./py3-runtime/build.sh
+cd py3-runtime
+./build.sh
+cd ..
