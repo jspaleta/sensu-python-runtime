93db68f606691293d8b9b9e22c84b768cffb9fe0
siva9427 <sivasukumarreddy.poreddy@objectrocket.com>
2021-02-01 11:03:25 +0530

    OR5-246 commit

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -1,12 +1,11 @@
 export ASSET_FILE_NAME=sensu-python-runtime_${ASSET_VERSION}_python-${PYTHON_VERSION}_${OSVERSION}_linux_amd64.tar.gz
 export S3_LOCATION=s3://${S3_BUCKET}${S3_FOLDER}
-export OSDISTRO=$(printf '%s' "$OSVERSION" | sed 's/[0-9]*//g')
 export IMAGE_TAG="sensu-python-runtime:${PYTHON_VERSION}-${OSVERSION}"
 export CONTAINER_NAME="sensu-python-runtime_${PYTHON_VERSION}-${OSVERSION}"
 export S3_FILE_URL=https://${S3_BUCKET}.s3.amazonaws.com${S3_FOLDER}${ASSET_FILE_NAME}
 export PATH=/var/lib/jenkins/.local/bin:$PATH
 pip install --user awscli
-docker build --build-arg "ASSET_VERSION=${ASSET_VERSION}" --build-arg "PYTHON_VERSION=${PYTHON_VERSION}" -t ${IMAGE_TAG} -f Dockerfile.${OSVERSION} .
+docker build --build-arg "ASSET_VERSION=${ASSET_VERSION}" --build-arg "PYTHON_VERSION=${PYTHON_VERSION}" -t $IMAGE_TAG -f Dockerfile.${OSVERSION} .
 docker run -itd --name ${CONTAINER_NAME} ${IMAGE_TAG}
 docker ${CONTAINER_NAME}:/assets/${ASSET_FILE_NAME} .
 docker rm -f ${CONTAINER_NAME}
