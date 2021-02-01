fcd600a3f312e45b907579d91e2697ad1fef11d6
siva9427 <sivasukumarreddy.poreddy@objectrocket.com>
2021-02-01 11:00:07 +0530

    OR5-246 commit

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -1,19 +1,17 @@
-ASSET_FILE_NAME=sensu-python-runtime_${ASSET_VERSION}_python-${PYTHON_VERSION}_${OSVERSION}_linux_amd64.tar.gz
-S3_LOCATION=s3://${S3_BUCKET}${S3_FOLDER}
-OSDISTRO=$(printf '%s' "$OSVERSION" | sed 's/[0-9]*//g')
-IMAGE_TAG="sensu-python-runtime:${PYTHON_VERSION}-${OSVERSION}"
-CONTAINER_NAME="sensu-python-runtime_${PYTHON_VERSION}-${OSVERSION}"
+export ASSET_FILE_NAME=sensu-python-runtime_${ASSET_VERSION}_python-${PYTHON_VERSION}_${OSVERSION}_linux_amd64.tar.gz
+export S3_LOCATION=s3://${S3_BUCKET}${S3_FOLDER}
+export OSDISTRO=$(printf '%s' "$OSVERSION" | sed 's/[0-9]*//g')
+export IMAGE_TAG="sensu-python-runtime:${PYTHON_VERSION}-${OSVERSION}"
+export CONTAINER_NAME="sensu-python-runtime_${PYTHON_VERSION}-${OSVERSION}"
 export S3_FILE_URL=https://${S3_BUCKET}.s3.amazonaws.com${S3_FOLDER}${ASSET_FILE_NAME}
-export OSDISTRO=$OSDISTRO
-pip install --user awscli
 export PATH=/var/lib/jenkins/.local/bin:$PATH
+pip install --user awscli
 docker build --build-arg "ASSET_VERSION=${ASSET_VERSION}" --build-arg "PYTHON_VERSION=${PYTHON_VERSION}" -t ${IMAGE_TAG} -f Dockerfile.${OSVERSION} .
 docker run -itd --name ${CONTAINER_NAME} ${IMAGE_TAG}
 docker ${CONTAINER_NAME}:/assets/${ASSET_FILE_NAME} .
 docker rm -f ${CONTAINER_NAME}
 echo "Uploading sensu asset to S3 bucket: ${S3_BUCKET}"
-SSUM=$(sha512sum ${ASSET_FILE_NAME} | cut -d ' ' -f 1)
-export SSUM=$SSUM
+export SSUM=$(sha512sum ${ASSET_FILE_NAME} | cut -d ' ' -f 1)
 aws s3 cp ${ASSET_FILE_NAME} ${S3_LOCATION} --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 ls -ltrh
 rm -rf ${ASSET_FILE_NAME}
