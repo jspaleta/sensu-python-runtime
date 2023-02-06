de9a2474f9bcd0f59ba967e21784036efebde361
Henry Finucane <hfinucane@zscaler.com>
2023-02-06 22:46:37 +0000

    Debian 10 can't build for Ubuntu 18.04, so drop it

--- build_platforms.sh
+++ build_platforms.sh
@@ -12,7 +12,7 @@ if [[ retval -ne 0 ]]; then
 fi
 
 # Debian8 platform
-platform="debian10" test_platforms="debian:10 debian:11 ubuntu:20.04 ubuntu:18.04" ./build_and_test_platform.sh
+platform="debian10" test_platforms="debian:10 debian:11 ubuntu:20.04" ./build_and_test_platform.sh
 retval=$?
 if [[ retval -ne 0 ]]; then
   exit $retval
